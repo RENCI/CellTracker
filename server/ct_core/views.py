c50c288307fb898dddbc0ff3b95dc1d5574c6a44
hyi <hongyi@renci.org>
2018-05-14 14:17:37 -0400

    get server up and running

--- server/ct_core/views.py
+++ server/ct_core/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.template import loader
+from django.conf import settings
 from django.http import HttpResponse
 
 
@@ -16,7 +17,11 @@ def index(request, session=''):
 
     template = loader.get_template('ct_core/index.html')
     context = {
-        'SITE_TITLE': settings.SITE_TITLE,
-        'status_msg': status_msg
+        'SITE_TITLE': settings.SITE_TITLE
     }
     return HttpResponse(template.render(context, request))
+
+
+def stream_video(request, path):
+    return HttpResponse("requested streaming path is " + path)
+
