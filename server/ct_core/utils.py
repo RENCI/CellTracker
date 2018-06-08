862a737cbbc5c2c422136a1a5e13c2a61a602951
hyi <hongyi@renci.org>
2018-06-08 17:58:46 -0400

    get image display and video streaming working

--- server/ct_core/utils.py
+++ server/ct_core/utils.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import os
+import shutil
 
 from django.conf import settings
 
@@ -17,14 +18,23 @@ def read_video(filename):
         time.sleep(settings.VIDEO_FRAME_INTERVAL_SECOND)
 
 
-def exact_images_from_video(videofile, imagepath):
+def extract_images_from_video(videofile, imagepath):
     cap = cv2.VideoCapture(videofile)
     success, frame = cap.read()
     count = 0
+    # clean up destination image path
+    if os.path.exists(imagepath):
+        shutil.rmtree(imagepath)
+
+    os.mkdir(imagepath)
+
     while success:
         ifile = os.path.join(imagepath, 'frame{}.jpg'.format(count))
         cv2.imwrite(ifile, frame)
         count += 1
         success, frame = cap.read()
-    ifile = os.path.join(imagepath, 'frame0.jpg')
-    return cv2.imread(ifile)
+
+    if count > 0:
+        return True
+    else:
+        return False
--- server/ct_core/views.py
+++ server/ct_core/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import os
 import shutil
+import cv2
 
 from django.template import loader
 from django.conf import settings
@@ -10,20 +11,26 @@ from django.http import HttpResponse, HttpResponseServerError, StreamingHttpResp
 
 from django.views.decorators import gzip
 
-from ct_core.utils import read_video, exact_images_from_video
+from ct_core.utils import read_video, extract_images_from_video
 from django_irods.storage import IrodsStorage
 
 
 # Create your views here.
-def index(request, session=''):
+def index(request):
     #import sys
     #sys.path.append("/home/docker/pycharm-debug")
     #import pydevd
-    #pydevd.settrace('152.54.9.92', port=21000, suspend=False)
+    #pydevd.settrace('172.17.0.1', port=21000, suspend=False)
 
     template = loader.get_template('ct_core/index.html')
+    image_file = os.path.join(settings.IRODS_ROOT, 'image', 'frame0.jpg')
+    if os.path.isfile(image_file):
+        extract = True
+    else:
+        extract = None
+
     context = {
-        'SITE_TITLE': settings.SITE_TITLE
+        'extract': extract
     }
     return HttpResponse(template.render(context, request))
 
@@ -48,7 +55,7 @@ def stream_video(request, exp_id):
     return HttpResponseServerError('iRODS server error')
 
 
-def display_images(request, exp_id):
+def extract_images(request, exp_id):
     # remove the temp video directory before streaming the new one
     image_path = os.path.join(settings.IRODS_ROOT, 'image')
     if os.path.exists(image_path):
@@ -58,5 +65,28 @@ def display_images(request, exp_id):
     for vfile in os.listdir(dpath):
         # there is supposed to be only one video
         vfilepath = os.path.join(dpath, vfile)
-        ret_img = exact_images_from_video(vfilepath, image_path)
-        return StreamingHttpResponse(ret_img, content_type="image/jpeg")
+        ret = extract_images_from_video(vfilepath, image_path)
+
+        template = loader.get_template('ct_core/index.html')
+        context = {
+            'SITE_TITLE': settings.SITE_TITLE,
+            'extract': True if ret else None
+        }
+        return HttpResponse(template.render(context, request))
+
+
+def display_images(request, frame_no):
+    img_name = 'frame{}.jpg'.format(frame_no)
+    ifile = os.path.join(settings.IRODS_ROOT, 'image', img_name)
+    if os.path.isfile(ifile):
+        return HttpResponse(open(ifile, 'rb'), content_type='image/jpg')
+        #template = loader.get_template('ct_core/image_display.html')
+        #context = {
+        #    'img_src': '/static/frames/' + img_name,
+        #    'img_text': 'Video frame ' + str(frame_no),
+        #    'next_frame_no': str(int(frame_no) + 1)
+        #}
+        #return HttpResponse(template.render(context, request))
+
+    else:
+        return HttpResponseServerError('The requested image frame does not exist on the server')
