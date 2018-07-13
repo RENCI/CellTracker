8ebd45461c5f9fbc344597209f600451c966d7cc
David Borland <borland@renci.org>
2018-07-13 11:19:04 -0400

    Use keyboard to control video playback, mouse to control tracing

--- server/ct_core/static/ct_core/js/sketch.js
+++ server/ct_core/static/ct_core/js/sketch.js
@@ -2,16 +2,21 @@ var numImages = 100;
 var expId = "16101015300";
 
 var images = [],
-    imageIndex = 0;
+    frame = 0,
+    maxFrame = 0;
 
-var play = true;
+var play = false
+    direction = "forward";
 
-var positions = [];
+var trace = false,
+    positions = [];
 
 function preload() {
   for (var i = 1; i <= numImages; i++) {
     images.push(loadImage("/display-image/" + expId + "/" + i));
   }
+
+  maxFrame = images.length - 1;
 }
 
 function setup() {
@@ -20,12 +25,16 @@ function setup() {
       aspect = im.width / im.height;
 
   createCanvas(windowWidth, windowWidth / aspect);
-  frameRate(5);
+  frameRate(2);
+  pause();
 
   // Resize images
   images.forEach(function(im) {
     im.resize(width, height);
   });
+
+  // Draw initial frame
+  redraw();
 }
 
 function windowResized() {
@@ -34,22 +43,24 @@ function windowResized() {
       aspect = im.width / im.height;
 
   resizeCanvas(windowWidth, windowWidth / aspect);
+
+  redraw();
 }
 
 function draw() {
-  if (!play) return;
+  // Get image
+  var im = images[frame];
 
-  // Draw image
-  var im = images[imageIndex];
+  if (trace) {
+    // Get normalized mouse position at end of last frame
+    var x = mouseX / (width - 1),
+        y = mouseY / (height - 1);
 
-  // Get normalized mouse position at end of last frame
-  var x = mouseX / width,
-      y = mouseY / height;
-
-  positions.push([
-    Math.max(0, Math.min(x, 1)),
-    Math.max(0, Math.min(y, 1))
-  ]);
+    positions.push([
+      Math.max(0, Math.min(x, 1)),
+      Math.max(0, Math.min(y, 1))
+    ]);
+  }
 
   image(im, 0, 0);
 
@@ -65,13 +76,65 @@ function draw() {
   }
 
   // Get next image
-  imageIndex++;
-  if (imageIndex >= images.length) {
-    imageIndex = 0;
-    positions = [];
+  if (play) {
+    frame++;
+    if (frame > maxFrame) {
+      play = !play;
+      frame = 0;
+    }
   }
 }
 
 function mouseClicked() {
+  trace = !trace;
+
+  if (trace) positions = [];
+
+  redraw();
+}
+
+function keyPressed() {
+  switch (keyCode) {
+    case 32:
+      // Space bar
+      togglePlay();
+      break;
+
+    case LEFT_ARROW:
+      frameForward();
+      break;
+
+    case RIGHT_ARROW:
+      frameBack();
+      break;
+  }
+}
+
+function play() {
+  play = true;
+  loop();
+}
+
+function pause() {
+  play = false;
+  noLoop();
+}
+
+function togglePlay() {
   play = !play;
+
+  if (play) loop();
+  else noLoop();
+}
+
+function frameForward() {
+  frame = Math.min(frame + 1, maxFrame);
+  pause();
+  redraw();
+}
+
+function frameBack() {
+  frame = Math.max(frame - 1, 0);
+  pause();
+  redraw();
 }
