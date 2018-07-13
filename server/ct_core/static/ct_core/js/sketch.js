2fd493b7995ebd2a4a27e644e32cdb368d082d05
David Borland <borland@renci.org>
2018-07-13 14:23:14 -0400

    Use p5 in insteance mode, which should enable us to integrate with React

--- server/ct_core/static/ct_core/js/sketch.js
+++ server/ct_core/static/ct_core/js/sketch.js
@@ -1,160 +1,165 @@
-var numImages = 100;
-var expId = "16101015300";
+var trace = new p5(function (s) {
+  var numImages = 100;
+  var expId = "16101015300";
 
-var images = [],
-    frame = 0,
-    maxFrame = 0;
+  var images = [],
+      frame = 0,
+      maxFrame = 0;
 
-var play = false
-    direction = "forward";
+  var play = false
+      direction = "forward";
 
-var trace = false,
-    positions = [];
+  var trace = false,
+      positions = [];
 
-function preload() {
-  for (var i = 1; i <= numImages; i++) {
-    images.push(loadImage("/display-image/" + expId + "/" + i));
+  s.preload = function() {
+    for (var i = 1; i <= numImages; i++) {
+      images.push(s.loadImage("/display-image/" + expId + "/" + i));
+    }
+
+    maxFrame = images.length - 1;
   }
 
-  maxFrame = images.length - 1;
-}
+  s.setup = function() {
+    // Get parent div
+    var div = s.select("#sketchDiv"),
+        w = innerWidth(div.elt);
 
-function setup() {
-  // Get parent div
-  var div = select("#sketchDiv"),
-      w = innerWidth(div.elt);
+    // Size canvas to image aspect ratio
+    var im = images[0],
+        aspect = im.width / im.height;
 
-  // Size canvas to image aspect ratio
-  var im = images[0],
-      aspect = im.width / im.height;
+    var canvas = s.createCanvas(w, w / aspect);
+    canvas.parent(div);
+    s.frameRate(2);
+    pause();
 
-  var canvas = createCanvas(w, w / aspect);
-  canvas.parent(div);
-  frameRate(2);
-  pause();
+    // Resize images
+    images.forEach(function(im) {
+      im.resize(s.width, s.height);
+    });
 
-  // Resize images
-  images.forEach(function(im) {
-    im.resize(width, height);
-  });
-
-  // Draw initial frame
-  redraw();
-}
-
-function windowResized() {
-  // Get parent div
-  var div = select("#sketchDiv"),
-      w = innerWidth(div.elt);
-
-  // Size canvas to image aspect ratio
-  var im = images[0],
-      aspect = im.width / im.height;
-
-  resizeCanvas(w, w / aspect);
-
-  redraw();
-}
-
-function draw() {
-  // Get image
-  var im = images[frame];
-
-  if (trace) {
-    // Get normalized mouse position at end of last frame
-    var x = mouseX / (width - 1),
-        y = mouseY / (height - 1);
-
-    positions.push([
-      Math.max(0, Math.min(x, 1)),
-      Math.max(0, Math.min(y, 1))
-    ]);
+    // Draw initial frame
+    s.redraw();
   }
 
-  image(im, 0, 0);
+  s.windowResized = function() {
+    // Get parent div
+    var div = s.select("#sketchDiv"),
+        w = innerWidth(div.elt);
 
-  // Draw path
-  strokeWeight(4)
-  for (var i = 1; i < positions.length; i++) {
-    var p0 = positions[i - 1],
-        p1 = positions[i];
+    // Size canvas to image aspect ratio
+    var im = images[0],
+        aspect = im.width / im.height;
 
-    stroke(127, 127, 127, i / (positions.length - 1) * 255);
+    s.resizeCanvas(w, w / aspect);
 
-    line(p0[0] * width, p0[1] * height, p1[0] * width, p1[1] * height);
+    s.redraw();
   }
 
-  // Get next image
-  if (play) {
-    frame++;
-    if (frame > maxFrame) {
-      play = !play;
-      frame = 0;
+  s.draw = function() {
+    // Get image
+    var im = images[frame];
+
+    if (trace) {
+      // Get normalized mouse position at end of last frame
+      var x = s.mouseX / (s.width - 1),
+          y = s.mouseY / (s.height - 1);
+
+      positions.push([
+        Math.max(0, Math.min(x, 1)),
+        Math.max(0, Math.min(y, 1))
+      ]);
+    }
+
+    s.image(im, 0, 0);
+
+    // Draw path
+    s.strokeWeight(4)
+    for (var i = 1; i < positions.length; i++) {
+      var p0 = positions[i - 1],
+          p1 = positions[i];
+
+      s.stroke(127, 127, 127, i / (positions.length - 1) * 255);
+
+      s.line(
+        p0[0] * (s.width - 1), p0[1] * (s.height - 1),
+        p1[0] * (s.width - 1), p1[1] * (s.height - 1)
+      );
+    }
+
+    // Get next image
+    if (play) {
+      frame++;
+      if (frame > maxFrame) {
+        play = !play;
+        frame = 0;
+      }
     }
   }
-}
 
-function mouseClicked() {
-  trace = !trace;
+  s.mouseClicked = function() {
+    trace = !trace;
 
-  cursor(trace ? CROSS : ARROW);
+    s.cursor(trace ? s.CROSS : s.ARROW);
 
-  if (trace) positions = [];
+    if (trace) positions = [];
 
-  redraw();
-}
-
-function keyPressed() {
-  switch (keyCode) {
-    case 32:
-      // Space bar
-      togglePlay();
-      break;
-
-    case LEFT_ARROW:
-      frameForward();
-      break;
-
-    case RIGHT_ARROW:
-      frameBack();
-      break;
+    s.redraw();
   }
-}
 
-function play() {
-  play = true;
-  loop();
-}
+  s.keyPressed = function() {
+    switch (s.keyCode) {
+      case 32:
+        // Space bar
+        togglePlay();
+        break;
 
-function pause() {
-  play = false;
-  noLoop();
-}
+      case s.LEFT_ARROW:
+        frameForward();
+        break;
 
-function togglePlay() {
-  play = !play;
+      case s.RIGHT_ARROW:
+        frameBack();
+        break;
+    }
+  }
 
-  if (play) loop();
-  else noLoop();
-}
+  function play() {
+    play = true;
+    s.loop();
+  }
 
-function frameForward() {
-  frame = Math.min(frame + 1, maxFrame);
-  pause();
-  redraw();
-}
+  function pause() {
+    play = false;
+    s.noLoop();
+  }
 
-function frameBack() {
-  frame = Math.max(frame - 1, 0);
-  pause();
-  redraw();
-}
+  function togglePlay() {
+    play = !play;
 
-function innerWidth(element) {
-  var cs = getComputedStyle(element);
+    if (play) s.loop();
+    else s.noLoop();
+  }
 
-  var padding = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight),
-      border = parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
+  function frameForward() {
+    frame = Math.min(frame + 1, maxFrame);
+    pause();
+    s.redraw();
+  }
 
-  return element.offsetWidth - padding - border;
-}
+  function frameBack() {
+    frame = Math.max(frame - 1, 0);
+    pause();
+    s.redraw();
+  }
+
+  function innerWidth(element) {
+    var cs = getComputedStyle(element);
+
+    var padding = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight),
+        border = parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
+
+    return element.offsetWidth - padding - border;
+  }
+}, "sketchDiv");
