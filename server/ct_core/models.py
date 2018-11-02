303f3a1c84b10d1eaf237523122c811b4bb8bb33
hyi <hongyi@renci.org>
2018-11-02 15:26:40 -0400

    added UserProfile model

--- server/ct_core/models.py
+++ server/ct_core/models.py
@@ -2,5 +2,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='user')
+    grade = models.PositiveSmallIntegerField(blank=True,
+                                             validators=[MaxValueValidator(12),
+                                                         MinValueValidator(1)])
+    school = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
