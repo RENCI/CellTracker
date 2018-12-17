82f16d18bd9c8015448e11df0d1a5f0d65aa46ae
David Borland <borland@renci.org>
2018-12-17 15:43:52 -0500

    More work on merging

--- client/app/p5/TraceSketch.js
+++ client/app/p5/TraceSketch.js
@@ -196,7 +196,7 @@ module.exports = function (sketch) {
 
           var r = handleRadius / scale;
 
-          region.vertices.forEach(function(vertex) {
+          region.vertices.forEach(function(vertex, i) {
             var v = scalePoint(vertex);
 
             if (vertex === handle) {
@@ -392,7 +392,7 @@ module.exports = function (sketch) {
           merge = merge.length > 0 ? merge[0] : null;
 
           if (merge && merge !== experiment.selectedRegion.region) {
-            var dilation = 1 / images[0].width;
+            var dilation = 1.1 / images[0].width;
 
             var selected = experiment.selectedRegion.region;
 
@@ -409,84 +409,155 @@ module.exports = function (sketch) {
               return insidePolygon(v, selectedDilate) ? null : v;
             });
 
-            // Merge points
-            var merged = [];
-            var mergeStart;
-            for (var i = 0; i < selectedValid.length; i++) {
-              var v1 = selectedValid[i];
-              var v2 = i === selectedValid.length - 1 ? selectedValid[0] : selectedValid[i + 1];
+            if (selectedValid.indexOf(null) === -1 || mergeValid.indexOf(null) === -1) {
+              // Get non-null vertices
+              selectedValid = selectedValid.filter(function(d) { return d !== null; });
+              mergeValid = mergeValid.filter(function(d) { return d !== null; });
 
-              if (v1) {
-                // Add this one
-                merged.push(v1);
+              // Find two closest point pairs and merge them
+              var pairs = [];
+              selectedValid.forEach(function(v1, i) {
+                mergeValid.forEach(function(v2, j) {
+                  pairs.push({
+                    i: i,
+                    j: j,
+                    d2: distance2(v1, v2)
+                  });
+                });
+              });
 
-                if (!v2) {
-                  // Find closest merge point
-                  var closest = mergeValid.reduce(function(p, c, i) {
-                    if (!c) return p;
+              pairs.sort(function(a, b) {
+                return a.d2 - b.d2;
+              });
 
-                    var x = v1[0] - c[0];
-                    var y = v1[1] - c[1];
-                    var d = x * x + y * y;
+              var pair1 = pairs[0];
+              var pair2;
+              for (var i = 1; i < pairs.length; i++) {
+                var p = pairs[i];
 
-                    return p === null || d < p.d ? {
-                      i: i,
-                      d: d
-                    } : p;
-                  }, null);
-
-                  mergeStart = closest.i;
-
+                if (p.i !== pair1.i && p.j !== pair1.j) {
+                  pair2 = p;
                   break;
                 }
               }
+
+              // Sort by lowest i index
+              if (pair1.i > pair2.i) {
+                var temp = pair1;
+                pair1 = pair2;
+                pair2 = temp;
+              }
+
+              function mergePoints(p1, p2) {
+                return [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2];
+              }
+
+              // Merge regions
+              var merged = [];
+              for (var i = 0; i < pair1.i; i++) {
+                merged.push(selectedValid[i]);
+              }
+
+              merged.push(mergePoints(selectedValid[pair1.i], mergeValid[pair1.j]));
+
+              // XXX: Refactor to reduce redundancy with above code
+              for (var i = 0; i < mergeValid.length; i++) {
+                var j = (i + pair1.j) % mergeValid.length;
+
+                if (j === pair2.j) break;
+
+                merged.push(mergeValid[j]);
+              }
+
+              merged.push(mergePoints(selectedValid[pair2.i], mergeValid[pair2.j]));
+
+              for (var i = pair2.i + 1; i < selectedValid.length; i++) {
+                merged.push(selectedValid[i]);
+              }
+
+              // Update the vertices
+              setVertices(selected, merged);
+
+              // Remove the other region
+              segmentationData[frame].splice(segmentationData[frame].indexOf(merge), 1);
             }
+            else {
+              // Merge regions
+              var merged = [];
+              var mergeStart;
+              for (var i = 0; i < selectedValid.length; i++) {
+                var v1 = selectedValid[i];
+                var v2 = i === selectedValid.length - 1 ? selectedValid[0] : selectedValid[i + 1];
 
-            // XXX: Refactor to reduce redundancy with above code
-            var selectedStart;
-            for (var i = 0; i < mergeValid.length; i++) {
-              var i1 = (i + mergeStart) % mergeValid.length;
-              var v1 = mergeValid[i1];
-              var v2 = i1 === mergeValid.length - 1 ? mergeValid[0] : mergeValid[i + 1];
+                if (v1) {
+                  // Add this one
+                  merged.push(v1);
 
-              if (v1) {
-                // Add this one
-                merged.push(v1);
+                  if (!v2) {
+                    // Find closest merge point
+                    var closest = mergeValid.reduce(function(p, c, i) {
+                      if (!c) return p;
 
-                if (!v2) {
-                  // Find closest merge point
-                  var closest = selectedValid.reduce(function(p, c, i) {
-                    if (!c) return p;
+                      var d2 = distance2(v1, c);
 
-                    var x = v1[0] - c[0];
-                    var y = v1[1] - c[1];
-                    var d = x * x + y * y;
+                      return p === null || d2 < p.d2 ? {
+                        i: i,
+                        d2: d2
+                      } : p;
+                    }, null);
 
-                    return p === null || d < p.d ? {
-                      i: i,
-                      d: d
-                    } : p;
-                  }, null);
+                    mergeStart = closest.i;
 
-                  selectedStart = closest.i;
-
-                  break;
+                    break;
+                  }
                 }
               }
+
+              // XXX: Refactor to reduce redundancy with above code
+              var selectedStart;
+              for (var i = 0; i < mergeValid.length; i++) {
+                var i1 = (i + mergeStart) % mergeValid.length;
+
+                var v1 = mergeValid[i1];
+                var v2 = i1 === mergeValid.length - 1 ? mergeValid[0] : mergeValid[i1 + 1];
+
+                if (v1) {
+                  // Add this one
+                  merged.push(v1);
+
+                  if (!v2) {
+                    // Find closest merge point
+                    var closest = selectedValid.reduce(function(p, c, i) {
+                      if (!c) return p;
+
+                      var d2 = distance2(v1, c);
+
+                      return p === null || d2 < p.d2 ? {
+                        i: i,
+                        d2: d2
+                      } : p;
+                    }, null);
+
+                    selectedStart = closest.i;
+
+                    break;
+                  }
+                }
+              }
+
+              for (var i = selectedStart; i < selectedValid.length; i++) {
+                var v = selectedValid[i];
+
+                if (v) merged.push(v);
+                else break;
+              }
+
+              // Update the vertices
+              setVertices(selected, merged);
+
+              // Remove the other region
+              segmentationData[frame].splice(segmentationData[frame].indexOf(merge), 1);
             }
-
-            for (var i = selectedStart; i < selectedValid.length; i++) {
-              var v = selectedValid[i];
-
-              if (v) merged.push(v);
-              else break;
-            }
-
-            // Update the vertices
-            setVertices(selected, merged);
-
-            // Remove the other region
-            segmentationData[frame].splice(segmentationData[frame].indexOf(merge), 1);
           }
         }
 
@@ -526,7 +597,7 @@ module.exports = function (sketch) {
               if (i === intersections[0].index) {
                 var p = intersections[0].point;
 
-                var x = normalize([p[0] - v[0], p[1] - v[1]]);
+                var x = normalizeVector([p[0] - v[0], p[1] - v[1]]);
                 x[0] *= offset;
                 x[1] *= offset;
 
@@ -538,7 +609,7 @@ module.exports = function (sketch) {
               else if (i === intersections[1].index) {
                 var p = intersections[1].point;
 
-                var x = normalize([p[0] - v[0], p[1] - v[1]]);
+                var x = normalizeVector([p[0] - v[0], p[1] - v[1]]);
                 x[0] *= offset;
                 x[1] *= offset;
 
@@ -717,12 +788,6 @@ module.exports = function (sketch) {
       ];
     }
 
-    function normalize(v) {
-      var m = Math.sqrt(v[0] * v[0] + v[1] * v[1]);
-
-      return m === 0 ? v : [v[0] / m, v[1] / m];
-    }
-
     function dilate(vertices, alpha) {
       return vertices.map(function(v, i, a) {
         // Get neighbors
@@ -730,8 +795,8 @@ module.exports = function (sketch) {
         var v2 = i === a.length - 1 ? a[0] : a[i + 1];
 
         // Get normals
-        var n1 = normalize([v[1] - v1[1], -(v[0] - v1[0])]);
-        var n2 = normalize([v2[1] - v[1], -(v2[0] - v[0])]);
+        var n1 = normalizeVector([v[1] - v1[1], -(v[0] - v1[0])]);
+        var n2 = normalizeVector([v2[1] - v[1], -(v2[0] - v[0])]);
         var n = [(n1[0] + n2[0]) / 2, (n1[1] + n2[1]) / 2];
 
         // Dilate
@@ -897,6 +962,52 @@ module.exports = function (sketch) {
     }
   }
 
+  function innerWidth(element) {
+    var cs = getComputedStyle(element);
+
+    var padding = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight),
+        border = parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
+
+    return element.offsetWidth - padding - border;
+  }
+
+  function applyZoom(p) {
+    var x = p[0] - translation[0] * scale;
+    x /= scale;
+
+    var y = p[1] - translation[1] * scale;
+    y /= scale;
+
+    return [x, y];
+  }
+
+  function normalizePoint(p) {
+    return [p[0] / sketch.width, p[1] / sketch.height];
+  }
+
+  function scalePoint(p) {
+    return [p[0] * sketch.width, p[1] * sketch.height];
+  }
+
+  // Various math/vector functions
+
+  function distance2(p1, p2) {
+    var x = p1[0] - p2[0];
+    var y = p1[1] - p2[1];
+
+    return x * x + y * y;
+  }
+
+  function vectorMagnitude(v) {
+    return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+  }
+
+  function normalizeVector(v) {
+    var mag = vectorMagnitude(v);
+
+    return mag === 0 ? [0, 0] : [v[0] / mag, v[1] / mag];
+  }
+
   // Adapted from: http://paulbourke.net/geometry/polygonmesh/
   function insidePolygon(p, polygon) {
     var n = polygon.length,
@@ -936,8 +1047,8 @@ module.exports = function (sketch) {
     }
 
     // Compute u
-    var u = ( (p[0] - p1[0]) * (p2[0] - p1[0]) + (p[1] - p1[1]) * (p2[1] - p1[1]) ) /
-            ( Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2) );
+    var u = ((p[0] - p1[0]) * (p2[0] - p1[0]) + (p[1] - p1[1]) * (p2[1] - p1[1])) /
+            (Math.pow(p2[0] - p1[0], 2) + Math.pow(p2[1] - p1[1], 2));
 
     // Test u
     if (u >= 0 && u <= 1) {
@@ -981,32 +1092,5 @@ module.exports = function (sketch) {
   	var y = p1[1] + ua * (p2[1] - p1[1]);
 
   	return [x, y];
-  }
-
-  function innerWidth(element) {
-    var cs = getComputedStyle(element);
-
-    var padding = parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight),
-        border = parseFloat(cs.borderLeftWidth) + parseFloat(cs.borderRightWidth);
-
-    return element.offsetWidth - padding - border;
-  }
-
-  function applyZoom(p) {
-    var x = p[0] - translation[0] * scale;
-    x /= scale;
-
-    var y = p[1] - translation[1] * scale;
-    y /= scale;
-
-    return [x, y];
-  }
-
-  function normalizePoint(p) {
-    return [p[0] / sketch.width, p[1] / sketch.height];
-  }
-
-  function scalePoint(p) {
-    return [p[0] * sketch.width, p[1] * sketch.height];
   }
 }
