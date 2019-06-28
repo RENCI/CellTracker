ed6ccc37c7cb752adc8f469e2d9ba7fe761b70ca
David Borland <borland@renci.org>
2019-06-28 11:37:55 -0400

    Fix issue with tooltips

--- client/app/hooks/useTooltip.js
+++ client/app/hooks/useTooltip.js
@@ -3,13 +3,16 @@ import React, { useEffect } from 'react';
 const useTooltip = ref => {
   useEffect(() => {
     // Enable tooltips
-    $(ref.current).find("[data-toggle='tooltip']").tooltip();
+    const tooltips = $(ref.current).find("[data-toggle='tooltip']");
+    tooltips.tooltip();
+    tooltips.tooltip("hide");
+    //$(ref.current).find("[data-toggle='tooltip']").tooltip();
 
     return () => {
       // Hide any open tooltips
       $(ref.current).find("[data-toggle='tooltip']").tooltip("hide");
     }
-  }, []);
+  });
 }
 
 export default useTooltip;
